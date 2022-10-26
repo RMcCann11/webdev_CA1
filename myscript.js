ae1a83bb18630c0c376d1647217b024f564d24f1
RMcCann11 <72083308+RMcCann11@users.noreply.github.com>
2022-10-26 09:40:11 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function MyFunction(){
+    if (document.getElementById("MyDiv").style.display
+    ==="none") {
+        document.getElementById("MyDiv").style.display
+    ="block";
+    } else{
+        document.getElementById("MyDiv").style.display
+    ="none";
+    }
+}
